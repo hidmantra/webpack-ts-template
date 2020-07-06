bbad69e8ab5508c56c90b669df86e8dd5a6687d6
Deriv Diggs <derivdiggs@gmail.com>
2020-07-06 19:01:44 -0400

    included overview and technologies in layout

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,8 @@ const mediumHolder: any = document.getElementById("medium-holder");
 
 //let scrollCntrl: CntrlActList;
 
-let scrollCntrl: any;
+let scrollCntrlA: any;
+let scrollCntrlB: any;
 let cntrlGroupAa:CntrlGroup;
 let cntrlGroupAb:CntrlGroup;
 let cntrlGroupAc:CntrlGroup;
@@ -36,6 +37,22 @@ const g1l2: JQuery = $("#g1l2");
 const g1l3: JQuery = $("#g1l3");
 const g1l4: JQuery = $("#g1l4");
 const g1l5: JQuery = $("#g1l5");
+
+
+let cntrlGroupBa:CntrlGroup;
+let cntrlGroupBb:CntrlGroup;
+let cntrlGroupBc:CntrlGroup;
+let cntrlGroupBd:CntrlGroup;
+let cntrlGroupBe:CntrlGroup;
+let cntrlGroupBf:CntrlGroup;
+
+const g2: JQuery = $("#g2");
+const g2l1: JQuery = $("#g2l1");
+const g2l2: JQuery = $("#g2l2");
+const g2l3: JQuery = $("#g2l3");
+const g2l4: JQuery = $("#g2l4");
+const g2l5: JQuery = $("#g2l5");
+const g2l6: JQuery = $("#g2l6");
 const headerLogo = new Image();
 const simpleLogo = new Image();
 const bigLogo = new Image();
@@ -83,16 +100,30 @@ $(document).ready(function () {
   cntrlGroupAc = new CntrlGroup({ start: 16, rest: 40, leave: 84, gone: 91 }, [{ attrDelta: "opacity", initial: 0, final: 1, end: 0 }, { attrDelta: "top", initial: 32, final: 30, end: 20 }]);
   cntrlGroupAd = new CntrlGroup({ start: 24, rest: 48, leave: 86, gone: 93 }, [{ attrDelta: "opacity", initial: 0, final: 1, end: 0 }, { attrDelta: "top", initial: 42, final: 40, end: 30 }]);
   cntrlGroupAe = new CntrlGroup({ start: 32, rest: 56, leave: 88, gone: 95 }, [{ attrDelta: "opacity", initial: 0, final: 1, end: 0 }, { attrDelta: "top", initial: 57, final: 55, end: 45 }]);
-
+ 
+  cntrlGroupBa = new CntrlGroup({ start: 1, rest: 24, leave: 80, gone: 87 }, [{ attrDelta: "opacity", initial: 0, final: 1, end: 0 }, { attrDelta: "top", initial: 11, final: 9, end: 0 }]);
+  cntrlGroupBb = new CntrlGroup({ start: 8, rest: 32, leave: 82, gone: 89 }, [{ attrDelta: "opacity", initial: 0, final: 1, end: 0 }, { attrDelta: "top", initial: 17, final: 15, end: 7 }]);
+  cntrlGroupBc = new CntrlGroup({ start: 16, rest: 40, leave: 84, gone: 91 }, [{ attrDelta: "opacity", initial: 0, final: 1, end: 0 }, { attrDelta: "top", initial: 24, final: 22, end: 14 }]);
+  cntrlGroupBd = new CntrlGroup({ start: 24, rest: 48, leave: 86, gone: 93 }, [{ attrDelta: "opacity", initial: 0, final: 1, end: 0 }, { attrDelta: "top", initial: 31, final: 29, end: 21 }]);
+  cntrlGroupBe = new CntrlGroup({ start: 32, rest: 56, leave: 88, gone: 95 }, [{ attrDelta: "opacity", initial: 0, final: 1, end: 0 }, { attrDelta: "top", initial: 38, final: 36, end: 28 }]);
+  cntrlGroupBf = new CntrlGroup({ start: 40, rest: 64, leave: 90, gone: 97 }, [{ attrDelta: "opacity", initial: 0, final: 1, end: 0 }, { attrDelta: "top", initial: 46, final: 44, end: 35 }]);
   let actSetAa: ActSet = new ActSet(g1l1, cntrlGroupAa);
   let actSetAb: ActSet = new ActSet(g1l2, cntrlGroupAb);
   let actSetAc: ActSet = new ActSet(g1l3, cntrlGroupAc);
   let actSetAd: ActSet = new ActSet(g1l4, cntrlGroupAd);
   let actSetAe: ActSet = new ActSet(g1l5, cntrlGroupAe);
-  let tmpActArr:Array<ActSet> = [actSetAa,actSetAb,actSetAc,actSetAd, actSetAe];
+  let actSetBa: ActSet = new ActSet(g2l1, cntrlGroupBa);
+  let actSetBb: ActSet = new ActSet(g2l2, cntrlGroupBb);
+  let actSetBc: ActSet = new ActSet(g2l3, cntrlGroupBc);
+  let actSetBd: ActSet = new ActSet(g2l4, cntrlGroupBd);
+  let actSetBe: ActSet = new ActSet(g2l5, cntrlGroupBe);
+  let actSetBf: ActSet = new ActSet(g2l6, cntrlGroupBf);
+  let tmpActArrA:Array<ActSet> = [actSetAa,actSetAb,actSetAc,actSetAd, actSetAe];
+  let tmpActArrB:Array<ActSet> = [actSetBa,actSetBb,actSetBc,actSetBd, actSetBe, actSetBf];
   //tmpActArr.push(actSetAa);
   //tmpActArr.push(actSetAb);
-  scrollCntrl = new CntrlActList(tmpActArr, 100, 200, 0);
+  scrollCntrlA = new CntrlActList(tmpActArrA, 100, 200, 0);
+  scrollCntrlB = new CntrlActList(tmpActArrB, 400, 200, 0);
 
   simpleLogo.src = SimpleLogo;
   simpleLogo.width = 50;
@@ -156,7 +187,9 @@ window.addEventListener("scroll", () => {
   windowScrollPosition = $(window).scrollTop();
   relativeScroll = (100 / $(window).innerHeight()) * windowScrollPosition;
   console.log("sp: " + windowScrollPosition + " rel sp: " + relativeScroll);
-  scrollCntrl.updateAtts(relativeScroll);
+  scrollCntrlA.updateAtts(relativeScroll);
+  scrollCntrlB.updateAtts(relativeScroll);
+
   if (windowScrollPosition > 1000) {
     let op: number = (2000 - windowScrollPosition) * .001;
     console.log("op: " + op);
