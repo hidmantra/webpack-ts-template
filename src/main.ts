2e4b2387c84d9b31f6226e30ab30f2029a5cb0c8
Deriv Diggs <derivdiggs@gmail.com>
2021-01-11 19:09:03 -0500

    code cleanup

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import * as $ from "jquery";
 import * as _ from "lodash";
 import { ActSet, CntrlActList, CntrlGroup, MPos, findPos } from './components/dUtils';
 import "bootstrap";
-import * as geometric from "../node_modules/geometric/build/geometric";
 import { GalleryStripModal } from "./components/gallery_strip_modal/GalleryStripModal";
 import SimpleLogo from "./img/THR_logo_bw.png";
 import HeaderLogo from "../src/img/tr.png";
@@ -10,24 +9,12 @@ import BigLogo from "../src/img/THR_Logo_knockout2.png";
 import ProfilePic from "../src/img/deriv.jpg";
 import SkyDivePic from "../src/img/deriv2.jpg";
 
-//import animateScrollTo from "../node_modules/animated-scroll-to/lib/animated-scroll-to";
-//const scrollTrack = require('scroll-track');
-//import scrollTrack from "../node_modules/scroll-track/src/scroll-element/index 
-const mainHolder: JQuery = $("#holder");
 const introAnchor: JQuery = $("#cityscape");
 const ppholder: JQuery = $("#profile-pic-holder");
 const sdholder: JQuery = $("#skyDive-pic-holder");
-const resTop: JQuery = $("#resume-top");
 const logoIntroAnchor: JQuery = $("#bigLogoHolder");
 const fullCover: JQuery = $("#fullCover");
-const photocarousel: JQuery = $("#photo-carousel");
-const sect1: JQuery = $("#overview-top");
-const sect2: JQuery = $("#technology-top");
-const sect3: JQuery = $("#portfolio-top");
-const sect4: JQuery = $("#resume-top");
 const mediumHolder: any = document.getElementById("multimedia");
-
-//let scrollCntrl: CntrlActList;
 
 let scrollCntrlA: any;
 let scrollCntrlB: any;
@@ -37,7 +24,6 @@ let cntrlGroupAc:CntrlGroup;
 let cntrlGroupAd:CntrlGroup;
 let cntrlGroupAe:CntrlGroup;
 
-const g1: JQuery = $("#g1");
 const g1l1: JQuery = $("#g1l1");
 const g1l2: JQuery = $("#g1l2");
 const g1l3: JQuery = $("#g1l3");
@@ -52,7 +38,6 @@ let cntrlGroupBd:CntrlGroup;
 let cntrlGroupBe:CntrlGroup;
 let cntrlGroupBf:CntrlGroup;
 
-const g2: JQuery = $("#g2");
 const g2l1: JQuery = $("#g2l1");
 const g2l2: JQuery = $("#g2l2");
 const g2l3: JQuery = $("#g2l3");
@@ -66,38 +51,16 @@ const profilePic = new Image();
 const skyDivePic = new Image();
 
 let relativeScroll: number;
-let myLogo: JQuery;
 let mPos = new MPos();
 let windowScrollPosition: number;
-let perimArr: Array<any> = [];
 let vLine: JQuery;
 let hLine: JQuery;
 let sqr1: JQuery;
 let sqr2: JQuery;
 let sqr3: JQuery;
 let sqr4: JQuery;
-let wHeight: number = window.innerHeight;
-let wWidth: number = window.innerWidth;
-let firstRun: boolean = true;
-let dDelay: number = 1;
 mPos.x = 0;
 mPos.y = 0;
-/*
-const screenCovered = () => {
-  // eslint-disable-next-line no-console
-  console.log("bingo");
-  $(".navba").hide();
-};
-const screenUncovered = () => {
-  $(".navbar").show();
-};
-const galleryStripModal: GalleryStripModal = new GalleryStripModal();
-galleryStripModal.CoverUp.on(screenCovered);
-galleryStripModal.CoverDown.on(screenUncovered);
-addComponents();
-
-*/
-
 
 
 /* prevents code from running until the entire document loads */
@@ -105,6 +68,7 @@ $(document).ready(function () {
   /// eslint-disable-next-line no-console
   console.log("document loaded");
 
+  /* set up animations */
   cntrlGroupAa = new CntrlGroup({ start: 1, rest: 24, leave: 80, gone: 87 }, [{ attrDelta: "opacity", initial: 0, final: 1, end: 0 }, { attrDelta: "top", initial: 11, final: 9, end: 0 }]);
   cntrlGroupAb = new CntrlGroup({ start: 8, rest: 32, leave: 82, gone: 89 }, [{ attrDelta: "opacity", initial: 0, final: 1, end: 0 }, { attrDelta: "top", initial: 22, final: 20, end: 10 }]);
   cntrlGroupAc = new CntrlGroup({ start: 16, rest: 40, leave: 84, gone: 91 }, [{ attrDelta: "opacity", initial: 0, final: 1, end: 0 }, { attrDelta: "top", initial: 32, final: 30, end: 20 }]);
@@ -130,18 +94,16 @@ $(document).ready(function () {
   let actSetBf: ActSet = new ActSet(g2l6, cntrlGroupBf);
   let tmpActArrA:Array<ActSet> = [actSetAa,actSetAb,actSetAc,actSetAd, actSetAe];
   let tmpActArrB:Array<ActSet> = [actSetBa,actSetBb,actSetBc,actSetBd, actSetBe, actSetBf];
-  //tmpActArr.push(actSetAa);
-  //tmpActArr.push(actSetAb);
   scrollCntrlA = new CntrlActList(tmpActArrA, 100, 200, 0);
   scrollCntrlB = new CntrlActList(tmpActArrB, 400, 200, 0);
 
+  /* set up images */
   simpleLogo.src = SimpleLogo;
   simpleLogo.width = 50;
 
   profilePic.src = ProfilePic;
   profilePic.width = 200;
   profilePic.id = "ppic";
-  //profilePic.className = "col";
 
   skyDivePic.src = SkyDivePic;
   skyDivePic.width = 200;
@@ -156,7 +118,6 @@ $(document).ready(function () {
 
   $(headerLogo).appendTo("#logo-holder");
   $(".main-title").html("Thought Render");
-  const topRow:JQuery = $("#overview-top-row");
   $(profilePic).appendTo(ppholder);
   $(skyDivePic).appendTo(sdholder);
   
@@ -165,25 +126,24 @@ $(document).ready(function () {
   sqr2 = $("<div class= 'container-fluid pattern-b bk-img q2'></div> ");
   sqr3 = $("<div class= 'container-fluid pattern-c bk-img q3'></div> ");
   sqr4 = $("<div class= 'container-fluid pattern-d bk-img q4'></div> ");
-  $(mainHolder).mousemove(function (event) {
-    updateMouse(event.pageX, event.pageY);
-  })
 
+
+  /* fade-out cover to reveal logo */
+  $(fullCover).animate({
+    opacity: .3
+  }, 5000);
+
+  /* add elements to page */
   $(sqr1).appendTo(introAnchor);
   $(sqr2).appendTo(introAnchor);
   $(sqr3).appendTo(introAnchor);
   $(sqr4).appendTo(introAnchor);
   $(vLine).appendTo(introAnchor);
   $(hLine).appendTo(introAnchor);
-  $(fullCover).animate({
-    opacity: .3
-  }, 5000);
+  
   addComponents();
 });
 
-function launchSection(sectionToLaunch: Function) {
-  sectionToLaunch();
-}
 
 window.addEventListener("resize", () => {
   //$(myLogo).delay(10).animate({ left: (fullCover.width() - myLogo.width()) / 2, top: (wHeight - myLogo.height()) / 2 });
@@ -191,15 +151,12 @@ window.addEventListener("resize", () => {
 });
 
 window.addEventListener("scroll", () => {
-  console.log("pos arr: " + findPos(document.getElementById("resume-top")));
   windowScrollPosition = $(window).scrollTop();
   relativeScroll = (100 / $(window).innerHeight()) * windowScrollPosition;
-  //console.log("sp: " + windowScrollPosition + " rel sp: " + relativeScroll);
-  //console.log("restop pos: " + ((document.getElementById("resume-top").getBoundingClientRect())).top);
-
+  /* tell aimation groups to update values */
   scrollCntrlA.updateAtts(relativeScroll);
   scrollCntrlB.updateAtts(relativeScroll);
-
+  /* fade out logo based on scroll position */
   if (windowScrollPosition > 1000) {
     let op: number = (10000 - windowScrollPosition) * .001;
     console.log("op: " + op);
@@ -209,6 +166,7 @@ window.addEventListener("scroll", () => {
   
   let resTopPos:number = ((document.getElementById("resume-top").getBoundingClientRect())).y
   let actRes:boolean = false;
+  /* horizontaly scroll cityscape behind resume based on scroll position */
   if(resTopPos < 0) {
     actRes = true;
     let newPos:number =((resTopPos)/2);
@@ -217,47 +175,6 @@ window.addEventListener("scroll", () => {
   };
 })
 
-function anim1() {
-  let itemCnt: Number = Number($(g1).children());
-  const offsetY: number = 5;
-  $(g1l1).addClass("reveal");
-  $(g1l2).addClass("reveal");
-}
-
-function moveCrosshairs(tmpX: number, tmpY: number) {
-
-  $(vLine).delay(dDelay).animate({ left: tmpX, top: 0, height: wHeight });
-  $(hLine).delay(dDelay).animate({ left: 0, top: tmpY, width: wWidth });
-  $(sqr1).delay(dDelay).animate({ left: 0, top: 0, width: tmpX, height: tmpY });
-  $(sqr2).delay(dDelay).animate({ left: tmpX, top: 0, width: wWidth - tmpX, height: tmpY });
-  $(sqr3).delay(dDelay).animate({ left: 0, top: tmpY, width: tmpX, height: wHeight - tmpY });
-  $(sqr4).delay(dDelay).animate({ left: tmpX, top: tmpY, width: wWidth - tmpX, height: wHeight - tmpY });
-  if (!firstRun) {
-    dDelay = 5000;
-  } else {
-    $(mainHolder).addClass("visible");
-    $(mainHolder).removeClass("invisible");
-    firstRun = false;
-  }
-}
-
-function updateMouse(tmpX: number, tmpY: number) {
-  mPos.x = tmpX;
-  mPos.y = tmpY;
-}
-
-function introAnimation() {
-  let tmpPoint: Array<number> = [];
-  for (let i = 0; i < 3601; i = i + 30) {
-    console.log("p: " + myLogo.width());
-    tmpPoint = geometric.pointRotate([((wWidth - (.7 * myLogo.width())) / 2), wHeight / 2], i, [wWidth / 2, wHeight / 2]);
-    let tmpX: number = Math.round(tmpPoint[0]);
-    let tmpY: number = Math.round(tmpPoint[1]);
-    perimArr.push(tmpPoint);
-    moveCrosshairs(tmpX, tmpY);
-    console.log("tmpPoint x: " + tmpX + " y: " + tmpY);
-  }
-}
 
 function addComponents(): void {
   const galleryStripModal: GalleryStripModal = new GalleryStripModal();
@@ -265,8 +182,6 @@ function addComponents(): void {
   galleryStripModal.appendComponent(mediumHolder);
 
   const screenCovered = () => {
-    // eslint-disable-next-line no-console
-    console.log("bingo");
     $(".navbar").hide();
   };
   const screenUncovered = () => {
